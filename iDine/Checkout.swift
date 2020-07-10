b301e343b1529792aef9cddd8fb85c61b65c7a26
Raivis <raivis.olehno@applikation.co.uk>
2020-07-10 14:52:51 +0100

    Added new payment methods. Design is in progress.

---
+++ iDine/Checkout.swift
@@ -0,0 +1,36 @@
+//
+//  Checkout.swift
+//  iDine
+//
+//  Created by Raivis Olehno on 10/07/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    @State private var paymentType = 0
+    
+    var body: some View {
+        VStack{
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0 ..< Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
